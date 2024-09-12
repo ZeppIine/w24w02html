57feed4f632ddf0ab1655bcffe61f364f8df6f6f
ZeppIine <kim0904min@gmail.com>
2024-09-12 13:56:51 +0900

    [Feat] 코틀린 스프링 프로젝트 생성

---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20180147/w24w02html/W24w02htmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20180147.w24w02html
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w02htmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
